d2d1e243ab0ac98abb05ca43e566ee52214445ea
tvey <tanyavoid@ya.ru>
2021-10-29 22:05:36 +0300

    Remove example

--- bpm/users/urls.py
+++ bpm/users/urls.py
@@ -1,9 +1,5 @@
 from django.urls import path
 
-from bpm.users.views import example
-
 app_name = 'users'
 
-urlpatterns = [
-    path('', view=example, name='example'),
-]
+urlpatterns = []
--- bpm/users/views.py
+++ bpm/users/views.py
@@ -1,5 +1,1 @@
 from django.shortcuts import render
-
-
-def example(request):
-    return render(request, 'example.html')
